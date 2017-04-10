8b173fadfc9431f25dd313f58210883075692a50
User <campa813@gmail.com>
2017-04-09 20:43:52 -0500

    Added a second test.

--- HelloWorldTests/UnitTest1.cs
+++ HelloWorldTests/UnitTest1.cs
@@ -8,12 +8,15 @@ namespace HelloWorldTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
-        {
+        public void Test5plus4Equals9() {
             HelloWorld.Sample hw = new HelloWorld.Sample();
-
             Assert.AreEqual(9, hw.AddXandY(5, 4));
+        }
 
+        [TestMethod]
+        public void Test3DividedBy3Equals1() {
+            HelloWorld.Sample hw = new HelloWorld.Sample();
+            Assert.AreEqual(1, hw.DivideXbyY(3, 3));
         }
     }
 }
